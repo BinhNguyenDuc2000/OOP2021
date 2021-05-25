43811247576b4c3c6a99bb4c472bee0033bd1054
bcoldx <76272856+bcoldx@users.noreply.github.com>
2021-05-25 20:41:00 +0700

    Fix

--- src/geometry/line/Line.java
+++ src/geometry/line/Line.java
@@ -24,6 +24,8 @@ public class Line {
 	
 	public Line(Point a,Point b) {
 		//To do
+		this.point1 = a;
+		this.vector = new Vector(a,b);
 		
 	}
 
@@ -51,41 +53,22 @@ public class Line {
 	}
 	public double getDistance(Point newPoint) {
 		//Lay khoang cach giua duong thang va mot diem
-		//To do
-		double distanceP;
-		double x1;
-		double x2;
-		
-		x1 = Math.sqrt(Math.pow(LineSegment.x, 2) + Math.pow(LineSegment.y, 2));
-		x2 = Math.abs(LineSegment.a * newPoint.getX() + LineSegment.b * newPoint.getY() + LineSegment.c);
-		distanceP = x2/x1;
-		
-		return distanceP;
-	}
-	
-	//With vector
-	public double getAngle(Vector newVector) {
-		//Lay goc giua duong thang va mot vector
-		return 90-vector.getAngleS(newVector);
+		if (this.getVector()==null)
+			return 0;
+		Vector newVector = Math3D.computeCrossProduct(new Vector(this.getPoint1(),newPoint), this.getVector());
+		double space = newVector.getLength();
+		double bottom = this.getVector().getLength();
+		return space/bottom;
 	}
 	
 	//With line
-	public double getAngle(Line newLine) {
-		//Lay goc giua 2 duong thang
-		return vector.getAngleS(newLine.getVector());
-	}
-	public double getDistance(Line newlLine) {
-		//Lay khoang cach giua 2 duong thang
-		//To do
-		double distanceL = 0;
-		
-		return distanceL;
-	}
-	public Point getIntersection(Line newLine) {
-		//Lay giao diem giua duong thang va mot duong thang, tra ve null neu khong co
-		//To do
-		
-		return null;
+	 
+	public boolean isSame(Line newLine) {
+		//Kiem tra 2 duong thang co trung nhau
+		if (this.getVector().isParallel(newLine.getVector()) && newLine.hasPoint(this.getPoint1())){
+			return true;
+		}
+		return false;
 	}
 	
 	
